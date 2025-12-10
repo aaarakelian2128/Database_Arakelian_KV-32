af0e88b2ddb30f37f76279a53e57a34c689bb5fa
aaarakelian2128 <aaarakelian2128@gmail.com>
2025-12-11 01:01:21 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-# Налаштування підключення до PostgreSQL.
-DB = {
-    "host": "localhost",
-    "port": 5432,
-    "dbname": "postgres",
-    "user": "postgres",
-    "password": "2310"
-}
